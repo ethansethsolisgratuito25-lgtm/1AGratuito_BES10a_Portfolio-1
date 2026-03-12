02b79904f47e796f469f8aa4659b6fda7767d28a
ethansethsolisgratuito25-lgtm <ethansethsolis.gratuito25@bicol-u.edu.ph>
2026-03-12 17:16:35 +0800

    Add files via upload

---
+++ 1AGratuito_BES10a_Portfolio#1.py
@@ -0,0 +1,18 @@
+# Variables and Data Types
+brand_name = "Common Ground"            # String
+location = "Legazpi Night Market"       # String
+shirts_to_sell = 25                     # Integer
+price_per_shirt = 150.50                # Float
+is_first_pop_up = True                  # Boolean
+
+# Simple calculation
+projected_revenue = shirts_to_sell * price_per_shirt
+
+# Output
+print("--- Pop-Up Shop Details ---")
+print("Brand:", brand_name)
+print("Location:", location)
+print("First time popping up?", is_first_pop_up)
+print("Inventory count:", shirts_to_sell)
+print("Price per item: P", price_per_shirt)
+print("Projected Revenue: P", projected_revenue)
