296e8b03e37f9ba37ee1f1556d188d455b424642
ethansethsolisgratuito25-lgtm <ethansethsolis.gratuito25@bicol-u.edu.ph>
2026-05-12 23:55:50 +0800

    Add files via upload

--- 1AGratuito_BES10a_Portfolio#1.py
+++ 1AGratuito_BES10a_Portfolio#1.py
@@ -2,17 +2,14 @@
 # Course: BES10a
 # Student: Ethan Seth S. Gratuito
 
-# --- Variables and Data Types ---
-brand_name = input("Enter brand name: ")               # String
-location = input("Enter pop-up location: ")            # String
-shirts_to_sell = int(input("Enter inventory count: ")) # Integer conversion
-price_per_shirt = float(input("Enter unit price: "))   # Float conversion
-is_first_pop_up = True                                 # Boolean
+brand_name = input("Enter brand name: ")
+location = input("Enter pop-up location: ")
+shirts_to_sell = int(input("Enter inventory count: "))
+price_per_shirt = float(input("Enter unit price: "))
+is_first_pop_up = True
 
-# Simple calculation
 projected_revenue = shirts_to_sell * price_per_shirt
 
-# Output
 print("\n--- Pop-Up Shop Details ---")
 print("Brand:", brand_name)
 print("Location:", location)
